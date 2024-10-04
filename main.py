1ee9e75263f55ce2770051481390b079d479295b
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2024-10-04 17:07:13 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    domain = request.form['domain']
+    url = "https://api.render.com/v1/services/srv-cohmstol5elc73cql8g0/custom-domains"
+    
+    payload = { "name": domain }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": "Bearer rnd_1WAbxx4iZFcBPJlkNTiLeLLE2R1z"
+    }
+    
+    response = requests.post(url, json=payload, headers=headers)
+    return response.text
+
+if __name__ == '__main__':
+    app.run(debug=True)
